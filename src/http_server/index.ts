3f5f3a1f96ca4997140c24923aa8d50128e6253a
Natalia Efimova <n.efimova@systtech.ru>
2025-05-18 00:35:41 +0500

    feat: change js to ts for http server moddule

--- src/http_server/index.ts
+++ src/http_server/index.ts
@@ -2,8 +2,18 @@ import * as fs from 'fs';
 import * as path from 'path';
 import * as http from 'http';
 
-export const httpServer = http.createServer(function (req, res) {
+export const httpServer = http.createServer(function (
+    req: http.IncomingMessage,
+    res: http.ServerResponse,
+) {
     const __dirname = path.resolve(path.dirname(''));
+
+    if (req.url === '/favicon.ico') {
+        res.writeHead(204);
+        res.end();
+        return;
+    }
+
     const file_path = __dirname + (req.url === '/' ? '/front/index.html' : '/front' + req.url);
     fs.readFile(file_path, function (err, data) {
         if (err) {
